69d32d3975643b1e94f92af7279c23f76931c8fc
Anna Savrova <annsavrova@gmail.com>
2024-07-30 19:36:28 +0700

    Refactor repository to use Spring Data JPA

--- src/main/java/ru/netology/hwspringboothibernate/controller/PersonController.java
+++ src/main/java/ru/netology/hwspringboothibernate/controller/PersonController.java
@@ -1,15 +1,16 @@
 package ru.netology.hwspringboothibernate.controller;
 
 import ru.netology.hwspringboothibernate.entity.Person;
+import ru.netology.hwspringboothibernate.entity.PersonId;
 import ru.netology.hwspringboothibernate.repository.PersonRepository;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 
     private final PersonRepository personRepository;
@@ -18,8 +19,33 @@ public class PersonController {
         this.personRepository = personRepository;
     }
 
-    @GetMapping("/persons/by-city")
+    @GetMapping("/by-city")
     public List<Person> getPersonsByCity(@RequestParam String city) {
         return personRepository.findByCityOfLiving(city);
+    }
+
+    @GetMapping("/by-age")
+    public List<Person> getPersonsByAge(@RequestParam int age) {
+        return personRepository.findByAgeLessThanOrderByAgeAsc(age);
+    }
+
+    @GetMapping("/by-name-and-surname")
+    public Optional<Person> getPersonByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
+        return personRepository.findByNameAndSurname(name, surname);
+    }
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @PutMapping
+    public Person updatePerson(@RequestBody Person person) {
+        return personRepository.save(person);
+    }
+
+    @DeleteMapping
+    public void deletePerson(@RequestBody PersonId personId) {
+        personRepository.deleteById(personId);
     }
 }
